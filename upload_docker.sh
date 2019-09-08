0fa324e1ccfc811c6929ae5c16c0052972ff0bf2
Riei Ishizeki <rishizek@gmail.com>
2019-09-08 18:16:07 +0900

    Rename 'ml_api' to 'ml-api'.

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,13 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=rishizek/ml_api:latest
+dockerpath=rishizek/ml-api:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login
-docker tag ml_api:latest rishizek/ml_api:latest
+docker tag ml-api:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
