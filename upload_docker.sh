cb9c5f7117535994f0a457a46de3123943448bf1
Riei Ishizeki <rishizek@gmail.com>
2019-09-08 17:27:40 +0900

    Complete uplad_docker.sh.

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=rishizek/ml_api:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag ml_api:latest rishizek/ml_api:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
